e227265361124b43ad4df36786703a5245c33e76
RocketLL <luc4sl33@gmail.com>
2018-12-04 17:55:16 +0900

    async

--- reserver_defunct/reserver.go
+++ reserver_defunct/reserver.go
@@ -8,24 +8,22 @@ import (
 	"net/http/cookiejar"
 	"os"
 	"strings"
-	"sync"
 	"time"
 )
 
 var cookieJar, _ = cookiejar.New(nil)
+
 var client = &http.Client{Jar: cookieJar}
 
 func main() {
-	fmt.Printf("Program starting at %s\n", time.Now().Format(time.RFC3339Nano))
-
-	tries := 25
-	var wg1, wg2 sync.WaitGroup
+	fmt.Printf("Program starting at %s\n", time.Now().Format(time.RFC850))
 
 	id := flag.String("id", "", "Username/ID")
 	pw := flag.String("pw", "", "Password")
 	sCode := flag.Int("seat", 84, "seat code")
 	tCode1 := flag.Int("time1", 1, "time code 1")
 	tCode2 := flag.Int("time2", 4, "time code 2")
+	debug := flag.Bool("debug", false, "debugging mode")
 
 	flag.Parse()
 
@@ -34,39 +32,43 @@ func main() {
 		os.Exit(1)
 	}
 
-	// tt := time.Now()
-	fmt.Println("Logging in...")
-	login(*id, *pw)
-	fmt.Printf("Logged in and beginning wait at %s\n", time.Now().Format(time.RFC3339Nano))
-
-	for i := 0; i < 1500; i++ {
-		if time.Now().Minute() == 59 && time.Now().Second() == 59 {
-			t := time.Now()
-			for j := 0; j < tries; j++ {
-				wg1.Add(1)
-				wg2.Add(1)
-				go func() {
-					defer wg1.Done()
-					reserve(*sCode, *tCode1)
-				}()
-				go func() {
-					defer wg2.Done()
-					reserve(*sCode, *tCode2)
-				}()
-				time.Sleep(50 * time.Millisecond)
-			}
-			fmt.Printf("\n%s\n", time.Since(t))
-
-			wg1.Wait()
-			wg2.Wait()
-
-			break
-		} else {
-			time.Sleep(50 * time.Millisecond)
-		}
+	t := time.Now()
+	if !*debug {
+		fmt.Println("Beginning delay.")
+		time.Sleep(55500 * time.Millisecond)
+		fmt.Printf("%s delay done.\n", time.Since(t))
 	}
 
-	fmt.Printf("Exiting program at %s", time.Now().Format(time.RFC3339Nano))
+	tt := time.Now()
+	fmt.Println("Logging in...")
+	login(*id, *pw)
+	fmt.Printf("Logged in at %s\n", time.Now().Format(time.RFC850))
+	fmt.Printf("%s to attempt login.\n", time.Since(tt))
+
+	ok := false
+	n := 1
+
+	ttt := time.Now()
+	for !ok && n <= 30 {
+		tttt := time.Now()
+		if reserve(*sCode, *tCode1) && reserve(*sCode, *tCode2) {
+			fmt.Printf("Reserved successfully at %s", time.Now().Format(time.RFC850))
+			ok = true
+			break
+		}
+		fmt.Printf("Request took %s\n", time.Since(tttt))
+		fmt.Printf("Failed, retrying... Attempt %d\n", n)
+		n++
+		time.Sleep(150 * time.Millisecond)
+	}
+	fmt.Printf("Requests complete in %s\n", time.Since(ttt))
+
+	if ok {
+		fmt.Println("Success!")
+		os.Exit(0)
+	} else {
+		fmt.Printf("Error... Exited in %s. Current time is %s.\n", time.Since(t), time.Now().Format(time.RFC850))
+	}
 }
 
 func login(id, pw string) *http.Response {
@@ -93,7 +95,7 @@ func login(id, pw string) *http.Response {
 	return res
 }
 
-func reserve(sCode, tCode int) {
+func reserve(sCode, tCode int) bool {
 	url := "http://hi.hana.hs.kr/SYSTEM_Plan/Lib_System/Lib_System_Reservation/reservation_exec.asp"
 
 	rawPayload := fmt.Sprintf("code=001&s_code=%d&t_code=%d", sCode, tCode)
@@ -106,7 +108,7 @@ func reserve(sCode, tCode int) {
 	res, err := client.Do(req)
 
 	if err != nil {
-		fmt.Printf("Something went wrong...")
+		fmt.Println("Something went wrong...")
 	}
 
 	defer res.Body.Close()
@@ -115,9 +117,10 @@ func reserve(sCode, tCode int) {
 	fmt.Println(string(body))
 
 	if strings.Contains(string(body), "되었습니다") {
-		fmt.Printf("Success at %s for seat %d, time %d\n", time.Now().Format(time.RFC3339Nano), sCode, tCode)
+		fmt.Println("Successful.")
+		return true
 	} else {
-		fmt.Printf("Error at %s for seat %d, time %d\n", time.Now().Format(time.RFC3339Nano), sCode, tCode)
+		fmt.Printf("Error... ")
+		return false
 	}
-	return
 }
